d02712060ae7389e8dfb3ddd25a58ddb46291664
Nguyen Huynh <35204932+hknd23@users.noreply.github.com>
2024-06-14 19:06:59 +0900

    edits to ensemble meta learners

--- R/meta_learners.R
+++ R/meta_learners.R
@@ -24,7 +24,6 @@
 #' @export
 #'
 #' @examples
-#' \donttest{
 #' # load dataset
 #' data(exp_data)
 #' #load SuperLearner package
@@ -40,6 +39,7 @@
 #'                                 nfolds = 5,
 #'                                 binary.outcome = TRUE)
 #'
+#' \donttest{
 #' # estimate CATEs with T Learner
 #' set.seed(123456)
 #' tlearner <- metalearner_ensemble(cov.formula = support_war ~ age + income +
