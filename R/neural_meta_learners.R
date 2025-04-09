0bdb621cc648f638ce40e53a93683434bd3fd4e3
Yang Yang <yyang016@outlook.com>
2025-04-08 22:59:40 -0400

    Update neural_meta_learners.R
    Updated with X Learner

--- R/neural_meta_learners.R
+++ R/neural_meta_learners.R
@@ -65,6 +65,25 @@
 #' print(tlearner_nn)
 #'                                   }
 #'
+#' # load dataset
+#' set.seed(123456)
+#' # estimate CATEs with X Learner
+#' xlearner_nn <- metalearner_deepneural(cov.formula = support_war ~ age +
+#'                                   income  +
+#'                                   employed  + job_loss,
+#'                                   data = exp_data,
+#'                                   treat.var = "strong_leader",
+#'                                   meta.learner.type = "X.Learner",
+#'                                   stepmax = 2e+9,
+#'                                   nfolds = 5,
+#'                                   algorithm = "rprop+",
+#'                                   hidden.layer = c(3),
+#'                                   linear.output = FALSE,
+#'                                   binary.outcome = FALSE)
+#'
+#' print(xlearner_nn)
+#'                                   }
+#'
 
 metalearner_deepneural <- function(data,
                               cov.formula,
@@ -104,6 +123,7 @@ metalearner_deepneural <- function(data,
                        style = 3,
                        width = 50,
                        char = "=")
+  if (meta.learner.type != "X.Learner"){
   for(f in 1:(length(folds))){
     if(f == 1){
       data1 <- data[c(folds[[5]], folds[[2]], folds[[3]], folds[[4]]),]
@@ -226,7 +246,132 @@ metalearner_deepneural <- function(data,
     Sys.sleep(.05)
     setTxtProgressBar(pb, f)
   }
-  close(pb)
+  close(pb)}
+    if(meta.learner.type == "X.Learner"){
+    pseudo_all <- matrix(NA, nrow(data), 2)
+    ID_pseudo <- 1:nrow(data)
+    pseudo_all <- cbind(pseudo_all, ID_pseudo)
+    
+    set.seed(123456)
+    
+    for(f in seq_along(folds)){
+      if(f == 1){
+        data1 <- data[c(folds[[5]], folds[[2]], folds[[3]], folds[[4]]),]
+        df_main <- data[folds[[1]],]
+      }
+      if(f == 2){
+        data1 <- data[c(folds[[1]], folds[[5]], folds[[3]], folds[[4]]),]
+        df_main <- data[folds[[2]],]
+      }
+      if(f == 3){
+        data1 <- data[c(folds[[1]], folds[[2]], folds[[5]], folds[[4]]),]
+        df_main <- data[folds[[3]],]
+      }
+      if(f == 4){
+        data1 <- data[c(folds[[1]], folds[[2]], folds[[3]], folds[[5]]),]
+        df_main <- data[folds[[4]],]
+      }
+      if(f == 5){
+        data1 <- data[c(folds[[1]], folds[[2]], folds[[3]], folds[[4]]),]
+        df_main <- data[folds[[5]],]
+      }
+      
+      df_aux <- data1
+      
+      s.formula <- paste0("d ~ ", paste0(covariates, collapse = " + "))
+      p_mod <- neuralnet::neuralnet(s.formula, data = df_aux, hidden = 3, 
+                                    algorithm = algorithm, linear.output = FALSE, stepmax = stepmax)
+      p_hat <- predict(p_mod, df_main[, covariates]) 
+      p_hat <- ifelse(p_hat < 0.025, 0.025, ifelse(p_hat > 0.975, 0.975, p_hat))
+      pseudo_all[, 2][df_main$ID] <- p_hat
+      
+      aux_1 <- df_aux[df_aux$d == 1, ]
+      aux_0 <- df_aux[df_aux$d == 0, ]
+      
+      m1_mod <- neuralnet::neuralnet(s.formula, data = aux_1, hidden = hidden.layer, algorithm = algorithm, linear.output = FALSE, stepmax = stepmax)
+      m0_mod <- neuralnet::neuralnet(s.formula, data = aux_0, hidden = hidden.layer, algorithm = algorithm, linear.output = FALSE, stepmax = stepmax)
+      
+      m1_hat <- predict(m1_mod, df_main[, covariates])
+      m0_hat <- predict(m0_mod, df_main[, covariates])
+      
+      df_main$y <- as.numeric(as.character(df_main$y))
+      
+      tau1 <- df_main[df_main$d == 1, "y"] - m0_hat[df_main$d == 1]
+      tau0 <- m1_hat[df_main$d == 0] - df_main[df_main$d == 0, "y"]
+      
+      pseudo_all[, 1][df_main$ID[df_main$d == 1]] <- tau1
+      pseudo_all[, 1][df_main$ID[df_main$d == 0]] <- tau0
+      
+      Sys.sleep(0.05)
+      setTxtProgressBar(pb, f)
+    }
+    
+    close(pb)
+    
+    # Extract pseudo-outcomes for treated and control groups
+    pseudo_tau1 <- pseudo_all[data$d==1,1]
+    pseudo_tau0 <- pseudo_all[data$d==0,1]
+    
+    # Ensure predictors match the number of pseudo-outcomes
+    X_train1 <- data[data$d == 1, covariates]
+    X_train0 <- data[data$d == 0, covariates]
+    
+    # Create training datasets
+    train_data1 <- data.frame(y = pseudo_tau1, X_train1)
+    train_data0 <- data.frame(y = pseudo_tau0, X_train0)
+    
+    # Train neural networks for pseudo-outcomes
+    a1 <- tryCatch({
+    tau1_mod <- neuralnet::neuralnet(y ~ .,
+                                     data = train_data1,
+                                     hidden = hidden.layer,
+                                     algorithm = algorithm,
+                                     linear.output = FALSE,
+                                     stepmax = stepmax)
+    score_tau1 <- predict(tau1_mod, data[, covariates])
+    a1 <- score_tau1
+    },error=function(e){
+      
+      mean_score <- mean(pseudo_all[,1])
+      score_tau1 <- rep.int(mean_score, times = nrow(data))
+      a1 <- score_tau1
+      return(a1)
+    })
+    
+    score_tau1 <- a1
+    
+    a0 <- tryCatch({
+    tau0_mod <- neuralnet::neuralnet(y ~ .,
+                                     data = train_data0,
+                                     hidden = hidden.layer,
+                                     algorithm = algorithm,
+                                     linear.output = FALSE,
+                                     stepmax = stepmax)
+    score_tau0 <- predict(tau0_mod, data[, covariates])
+    a0 <- score_tau0
+    },error=function(e){
+      mean_score <- mean(pseudo_all[,1])
+      score_tau0 <- rep.int(mean_score, times = nrow(data))
+      a0 <- score_tau0
+      return(a0)
+    })
+    
+    score_tau0 <- a0
+    
+    score_X <- pseudo_all[, 2] * score_tau0 + (1 - pseudo_all[, 2]) * score_tau1
+    
+    score_meta[, 1] <- score_X
+    
+    learner_out <- list("formula" = cov.formula,
+                        "treat_var" = treat.var,
+                        "CATEs" = score_meta,
+                        "Y_hats" = data.frame("Y_hat0" = score_tau0, "Y_hat1" = score_tau1),
+                        "Meta_Learner" = meta.learner.type,
+                        "Prop_score" = pseudo_all[, 2],
+                        "algorithm" = algorithm,
+                        "hidden_layer" = hidden.layer,
+                        "data" = data)
+  }
   class(learner_out) <- "metalearner_deepneural"
   return(learner_out)
 }
