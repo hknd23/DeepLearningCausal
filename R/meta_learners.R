41fc907be29ee5d025b21b70c8cbb8cac5618992
Nguyen Huynh <35204932+hknd23@users.noreply.github.com>
2025-05-02 21:05:15 +0900

    Update meta_learners.R

--- R/meta_learners.R
+++ R/meta_learners.R
@@ -20,8 +20,8 @@
 #' @param binary.outcome logical specifying predicted outcome variable will take
 #' binary values or proportions.
 #'
-#' @return `metalearner_ensemble` of predicted outcome values and CATEs estimated by the meta
-#' learners for each observation.
+#' @return `metalearner_ensemble` of predicted outcome values and CATEs 
+#' estimated by the meta learners for each observation.
 #' @export
 #'
 #' @examples
@@ -60,11 +60,13 @@
 #'\donttest{
 #'# estimate CATEs with X Learner
 #'set.seed(123456)
-#' xlearner <- metalearner_ensemble(cov.formula = support_war ~ age + income + employed  + job_loss,
+#' xlearner <- metalearner_ensemble(cov.formula = support_war ~ age + income +
+#'  employed  + job_loss,
 #'                                  data = exp_data,
 #'                                  treat.var = "strong_leader",
 #'                                  meta.learner.type = "X.Learner",
-#'                                  SL.learners = c("SL.glmnet","SL.xgboost", "SL.ranger","SL.nnet"),
+#'                                  SL.learners = c("SL.glmnet","SL.xgboost", 
+#'                                  "SL.ranger","SL.nnet"),
 #'                                  nfolds = 5,
 #'                                  binary.outcome = TRUE)
 #' 
@@ -81,7 +83,8 @@ metalearner_ensemble <- function(data,
                                  nfolds = 5,
                                  binary.outcome = FALSE)
 {
-  if(meta.learner.type %in% c("S.Learner","T.Learner","X.Learner") == FALSE)
+  if(meta.learner.type %in% c("S.Learner", "T.Learner", 
+                              "X.Learner", "R.Learner") == FALSE)
   {
     stop("Meta Learner not supported")
   }
@@ -104,7 +107,8 @@ metalearner_ensemble <- function(data,
   
   folds <- caret::createFolds(data$d, k=nfolds)
   message("Training model for meta learner")
-  if (meta.learner.type != "X.Learner"){
+  
+  if (meta.learner.type %in% c("S.Learner", "T.Learner")){
     for(f in 1:(length(folds))){
       pb <- txtProgressBar(min = 0,
                            max = length(folds),
@@ -158,8 +162,10 @@ metalearner_ensemble <- function(data,
         X_test_1 <- (df_main[,c(covariates, "d")])
         X_test_1$d <- 1
         
-        Y_test_0 <- predict(object = m_mod, newdata = X_test_0, onlySL = TRUE)$pred
-        Y_test_1 <- predict(object = m_mod, newdata = X_test_1, onlySL = TRUE)$pred
+        Y_test_0 <- predict(object = m_mod, newdata = X_test_0, 
+                            onlySL = TRUE)$pred
+        Y_test_1 <- predict(object = m_mod, newdata = X_test_1, 
+                            onlySL = TRUE)$pred
         
         if (binary.outcome) {
           Y.pred.1p <- data.frame("outcome" = df_main$y,
@@ -277,6 +283,7 @@ metalearner_ensemble <- function(data,
       setTxtProgressBar(pb, f)
     }
     close(pb)}
+  
   if (meta.learner.type == "X.Learner") {
     message("Training X-Learner")
     
@@ -324,7 +331,8 @@ metalearner_ensemble <- function(data,
                                           cvControl = control)
       
       p_hat <- p_mod$SL.predict
-      p_hat <- ifelse(p_hat < 0.025, 0.025, ifelse(p_hat > 0.975, 0.975, p_hat)) # Overlap bounding
+      # Overlap bounding
+      p_hat <- ifelse(p_hat < 0.025, 0.025, ifelse(p_hat > 0.975, 0.975, p_hat)) 
       
       pseudo_all[, 2][df_main$ID] <- p_hat
       
@@ -365,7 +373,6 @@ metalearner_ensemble <- function(data,
       Sys.sleep(0.05)
       setTxtProgressBar(pb, f)
     }
-    
     
     close(pb)
     
@@ -418,10 +425,128 @@ metalearner_ensemble <- function(data,
                         "Y_hats" = data.frame("Y_hat0" = score_tau0, 
                                               "Y_hat1" = score_tau1),
                         "Meta_Learner" = meta.learner.type,
-                        "Prop_score"=pseudo_all[, 2],
+                        "Prop_score" = pseudo_all[, 2],
                         "SL_learners" = SL.learners,
                         "data" = data)
   }
+  
+  if (meta.learner.type == "R.Learner") {
+    message("Training R-Learner")
+    
+    data$ID <- c(1:nrow(data))
+    pseudo_all <- matrix(NA, nrow(data), 2)
+    ID_pseudo <- 1:nrow(data)
+    pseudo_all <- cbind(pseudo_all, ID_pseudo)
+    
+    ##### # 5-fold sample splitting
+    # Sample splitting
+    set.seed(1234)
+    folds <- caret::createFolds(data$d, k = 5)
+    for(f in 1:(length(folds))){
+      if(f == 1){
+        data1 <- data[c(folds[[5]], folds[[2]], folds[[3]], folds[[4]]),]
+        df_main <- data[folds[[1]],]
+      } 
+      if(f == 2){
+        data1 <- data[c(folds[[1]], folds[[5]], folds[[3]], folds[[4]]),]
+        df_main <- data[folds[[2]],]
+      } 
+      if(f == 3){
+        data1 <- data[c(folds[[1]], folds[[2]], folds[[5]], folds[[4]]),]
+        df_main <- data[folds[[3]],]
+      } 
+      if(f == 4){
+        data1 <- data[c(folds[[1]], folds[[2]], folds[[3]], folds[[5]]),]
+        df_main <- data[folds[[4]],]
+      } 
+      if(f == 5){
+        data1 <- data[c(folds[[1]], folds[[2]], folds[[3]], folds[[4]]),]
+        df_main <- data[folds[[5]],]
+      } 
+      
+      df_aux <- data1
+      ## R-learner 
+      # Train a classification model to get the propensity scores
+      p_mod <- SuperLearner(Y = df_aux$d, 
+                            X = df_aux[, covariates], 
+                            newX = df_main[, covariates], 
+                            SL.library = SL.learners,
+                            verbose = FALSE, 
+                            method = "method.NNLS", 
+                            family = binomial(),
+                            cvControl = control)
+      
+      p_hat <- p_mod$SL.predict
+      p_hat = ifelse(p_hat < 0.025, 0.025, 
+                     ifelse(p_hat > .975, .975, p_hat))
+      # Train a regression model 
+      m_mod <- SuperLearner(Y = df_aux$y,
+                            X = df_aux[,covariates], 
+                            newX = df_main[,covariates],
+                            SL.library = SL.learners,
+                            verbose = FALSE,
+                            method = "method.NNLS",
+                            family = ifelse(binary.outcome, 
+                                            "binomial", 
+                                            "gaussian"),
+                            cvControl = control)
+      m_hat <- m_mod$SL.predict
+      
+      # Apply the R-learner (residual-on-residual approach)
+      y_tilde = df_main$y - m_hat
+      w_tilde = df_main$d - p_hat
+      pseudo_outcome = y_tilde/w_tilde
+      
+      weights = w_tilde^2
+      ## Collect all pseudo outcomes
+      pseudo_all[,1][df_main$ID] <- pseudo_outcome
+      pseudo_all[,2][df_main$ID] <- weights
+    }
+    
+    res_combined_r <- matrix(NA,nrow(data),5)
+    
+    for(l in 1:10){
+      set <- seq.int(from=1, to=nrow(data)+1, length.out = 11)
+      if(l <= 5){
+        r_mod_cf <- SuperLearner(Y = pseudo_all[(set[l]:set[l+1]-1), 2],
+                                 X = data[(set[l]:set[l+1]-1),covariates], 
+                                 newX = data[(set[6]:set[11] - 1), covariates], 
+                                 SL.library = SL.learners,
+                                 verbose = FALSE, 
+                                 method = "method.NNLS",
+                                 obsWeights = pseudo_all[(set[l]:set[l+1]-1),3],
+                                 cvControl = control)
+        
+        score_r_1_cf <- r_mod_cf$SL.predict
+        res_combined_r[(set[6]:set[11] - 1), l] <- score_r_1_cf
+      }
+      if(l  >5){
+        r_mod_cf <- SuperLearner(Y = pseudo_all[(set[l]:set[l + 1] - 1), 2],
+                                 X = data[(set[l]:set[l + 1] - 1), covariates], 
+                                 newX = data[(set[1]:set[6] - 1), covariates], 
+                                 SL.library = SL.learners,
+                                 verbose = FALSE, 
+                                 method = "method.NNLS",
+                                 obsWeights = pseudo_all[(set[l]:set[l+1]-1),3],
+                                 cvControl = control)
+        
+        score_r_0_cf <- r_mod_cf$SL.predict
+        res_combined_r[(set[1]:set[6] - 1), (l - 5)] <- score_r_0_cf
+      }
+    }
+    score_meta <- rowMeans(res_combined_r)
+    
+    learner_out <- list("formula" = cov.formula,
+                        "treat_var" = treat.var,
+                        "CATEs" = score_meta,
+                        "Y_hats" = list("Y_hat0" = score_r_0_cf, 
+                                        "Y_hat1" = score_r_1_cf),
+                        "Meta_Learner" = meta.learner.type,
+                        "Prop_score" = pseudo_all[, 2],
+                        "SL_learners" = SL.learners,
+                        "data" = data)
+  }
+  
   class(learner_out) <- "metalearner_ensemble"
   return(learner_out)
 }
