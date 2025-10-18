8250029b96daade071699824a46c36032dff1cf4
Nguyen Huynh <35204932+hknd23@users.noreply.github.com>
2025-10-18 16:36:06 +0900

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,8 @@
 # This file is used to run code when loading the package
 .onAttach <- function(libname, pkgname) {
-  if (requireNamespace("reticulate", quietly = TRUE)) {
     if (!reticulate::py_available(initialize = FALSE)) {
       packageStartupMessage(
         "Install Python from https://www.python.org/ and use `python_ready()` to enable all features."
       )
     }
-  } else {
-    packageStartupMessage(
-      "The 'reticulate' package is not installed. Python-related features will be disabled.\n",
-      "Install with install.packages(\"reticulate\")."
-    )
-  }
-}
+  } 
