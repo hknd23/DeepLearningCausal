9a08c7b3b9a36d34b56536cdb812fe1ca2f27dad
bumbamukherjee <60080865+bumbamukherjee@users.noreply.github.com>
2025-10-27 21:19:11 -0400

    Update deep_meta_learners.R

--- R/deep_meta_learners.R
+++ R/deep_meta_learners.R
@@ -4,7 +4,7 @@
 #' CATEs using Deep Neural Networks through Tensorflow.
 #' Deep Learning Estimation of CATEs from four meta-learner models (S,T,X and R-learner) using TensorFlow and Keras3
 #' @param data data.frame object of data.
-If a single dataset is specified, then the model will use cross-validation to train the meta-learners and estimate CATEs.
+#' If a single dataset is specified, then the model will use cross-validation to train the meta-learners and estimate CATEs.  with their test daUsers can also specify the arguments (defined below) to separately train meta-learners on their training data and estiamte CATEs with their test data.
 #' @param train.data data.frame object of training data for Train/Test mode.
 #' @param test.data data.frame object of test data for Train/Test mode.
 #' @param cov.formula formula description of the model y ~ x(list of covariates).
